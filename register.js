c802f8c8d921d320622f7ad5cc3a2cc923b2ef89
Ragosjehil <ragosjehil91@gmail.com>
2025-05-24 23:30:15 +0800

    Update register.js

--- register.js
+++ register.js
@@ -1,46 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+<script type="module">
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+    import {
+      getAuth,
+      createUserWithEmailAndPassword,
+      signInWithEmailAndPassword,
+      onAuthStateChanged,
+      signOut,
+    } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+    // Your Firebase config
+    const firebaseConfig = {
+      apiKey: "AIzaSyAvqUEeadXULepbEuo77TxqEv8EmoZR0Z0",
+      authDomain: "smartcart-c1c20.firebaseapp.com",
+      projectId: "smartcart-c1c20",
+      storageBucket: "smartcart-c1c20.firebasestorage.app",
+      messagingSenderId: "703850020571",
+      appId: "1:703850020571:web:4e7793246bd720e5d116fe",
+      measurementId: "G-ZW53LF1Y19",
+    };
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDjUixl72E6Q_ikZndW3q9NtFzIZDi8kuc",
-    authDomain: "smart-cart-67577.firebaseapp.com",
-    projectId: "smart-cart-67577",
-    storageBucket: "smart-cart-67577.firebasestorage.app",
-    messagingSenderId: "53620435226",
-    appId: "1:53620435226:web:691c0cf81fdb995ac5e4a3",
-    measurementId: "G-0E8W96SM3Z"
-  };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-//submit button 
-
-const submit = document.getElementById('submit');
-submit.addEventListener("click", function (event) {
-  event.preventDefault()
-
-  //inputs
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed up 
-      const user = userCredential.user;
-      alert("Creating account...")
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage)
-      // ..
-    });
-})
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
